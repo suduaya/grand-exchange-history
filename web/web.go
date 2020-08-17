9d386c918bb991c3bf028ad4f4dea65ca0108251
suduaya <andrenascimento@ua.pt>
2020-08-17 11:46:31 +0100

    Added item search form

--- web/web.go
+++ web/web.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"fmt"
 	"github.com/foolin/gin-template"
 	"github.com/gin-gonic/gin"
 	"grand-exchange-history/item"
@@ -16,6 +17,15 @@ func Start() {
 	router.GET("/", func(ctx *gin.Context) {
 		//render only file, must full name with extension
 		items := item.SelectAllItems()
+		ctx.HTML(http.StatusOK, "../../web/views/page.html", gin.H{"title": "Page file title!!", "items": items})
+	})
+
+	router.POST("/", func(ctx *gin.Context) {
+		ctx.Request.ParseForm()
+		for key, value := range ctx.Request.PostForm {
+			fmt.Println(key, value)
+		}
+		items := []item.ItemNameId{}
 		ctx.HTML(http.StatusOK, "../../web/views/page.html", gin.H{"title": "Page file title!!", "items": items})
 	})
 
