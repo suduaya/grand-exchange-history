270d0417c0e12b774f467bd5295b07743cbc2d61
suduaya <andrenascimento@ua.pt>
2020-08-22 11:45:17 +0100

    Buy, sell and margin info

--- item/item.go
+++ item/item.go
@@ -44,6 +44,40 @@ type Item struct {
 	OverallQtd float64 `json:"overall_qtd"`
 }
 
+func GetItemById(item_id float64) []Item {
+	var allret []Item
+	api_endpoint := "https://rsbuddy.com/exchange/summary.json"
+
+	resp, err := http.Get(api_endpoint)
+	if err != nil {
+		// handle error
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	var event map[string]interface{}
+	json.Unmarshal(body, &event)
+	for _, v := range event {
+		ret := Item{
+			Id:         v.(map[string]interface{})["id"].(float64),
+			Name:       v.(map[string]interface{})["name"].(string),
+			Buying:     v.(map[string]interface{})["buy_average"].(float64),
+			Selling:    v.(map[string]interface{})["sell_average"].(float64),
+			Margin:     v.(map[string]interface{})["buy_average"].(float64) - v.(map[string]interface{})["sell_average"].(float64),
+			Overall:    v.(map[string]interface{})["overall_average"].(float64),
+			BuyingQtd:  v.(map[string]interface{})["buy_quantity"].(float64),
+			SellingQtd: v.(map[string]interface{})["sell_quantity"].(float64),
+			OverallQtd: v.(map[string]interface{})["overall_quantity"].(float64),
+		}
+
+		if ret.Id == item_id {
+			allret = append(allret, ret)
+		}
+
+	}
+	fmt.Println(allret)
+	return allret
+}
+
 func ItemNameContains(item_name string) []Item {
 	var allret []Item
 	api_endpoint := "https://rsbuddy.com/exchange/summary.json"
--- web/web.go
+++ web/web.go
@@ -6,6 +6,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"grand-exchange-history/item"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func Start() {
@@ -22,19 +24,34 @@ func Start() {
 
 	router.POST("/", func(ctx *gin.Context) {
 		ctx.Request.ParseForm()
+		var item_name string
 		for key, value := range ctx.Request.PostForm {
 			fmt.Println(key, value)
+			for _, v := range value {
+				item_name += v
+			}
 		}
-		items := []item.ItemNameId{}
-		ctx.HTML(http.StatusOK, "../../web/views/page.html", gin.H{"title": "Page file title!!", "items": items})
+
+		items := item.SelectAllItems()
+		var ret_items []item.ItemNameId
+		for _, item := range items {
+			if strings.Contains(strings.ToLower(item.Name), strings.ToLower(item_name)) {
+				ret_items = append(ret_items, item)
+			}
+		}
+		ctx.HTML(http.StatusOK, "../../web/views/page.html", gin.H{"title": "Page file title!!", "items": ret_items})
 	})
 
 	router.GET("/graph/:id/weekly", func(ctx *gin.Context) {
 		id := ctx.Param("id")
 
+		s, _ := strconv.ParseFloat(id, 64)
+		item_struct := item.GetItemById(s)[0]
+		fmt.Println("item_struct: ", item_struct)
+
 		x, y := item.LoadItemPriceHistory(id, 7)
 		items := []item.ItemNameId{}
-		ctx.HTML(http.StatusOK, "../../web/views/graph.html", gin.H{"id": id, "items": items, "x": x, "y": y})
+		ctx.HTML(http.StatusOK, "../../web/views/graph.html", gin.H{"id": id, "items": items, "x": x, "y": y, "item_struct": item_struct})
 	})
 
 	router.GET("/graph/:id/monthly", func(ctx *gin.Context) {
